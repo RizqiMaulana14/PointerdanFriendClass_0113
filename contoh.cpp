26b70c716d6309e95563934658c34ffd42ddce14
RizqiMaulana14 <maulanafitrah14@gmail.com>
2025-05-06 18:39:56 +0700

    Penulisan int main

--- contoh.cpp
+++ contoh.cpp
@@ -32,3 +32,14 @@ class Lingkaran{
         return 3.14 * r * r;
     }
 };
+
+int main(){
+    PersegiPanjang objekPP;            //Membuat Objek
+    Lingkaran ol;
+
+    cout << "Masukkan Panjang : ";
+    cin >> objekPP.panjang;
+    cout << "Masukkan Lebar : ";
+    cin >> objekPP.lebar;
+    cout << "Luas Persegi Panjang : " << objekPP.luasPp(objekPP.panjang, objekPP.lebar) << endl;
+}
