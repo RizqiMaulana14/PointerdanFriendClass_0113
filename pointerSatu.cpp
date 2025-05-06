45343a14b6c96ff0cb95521d6206758344ecaf7b
RizqiMaulana14 <maulanafitrah14@gmail.com>
2025-05-06 18:47:36 +0700

    Membuat Int Main

--- pointerSatu.cpp
+++ pointerSatu.cpp
@@ -8,3 +8,16 @@ class mahasiswa{
         cout << "No Induk : " << nim << endl;
     }
 };
+
+int main (){
+    mahasiswa mhs{1};       //Objek mhs
+    mhs.showNim();          //Member Acces Operator
+
+    mahasiswa &refMhs = mhs;    //Pointer Reference refMhs
+    refMhs.nim = 2;             //Member Acces Operator
+
+    mahasiswa *pMhs = &mhs;     //Pointer Deference pMhs
+    pMhs ->nim = 3;             // Arrow Operator
+    pMhs ->showNim();
+    return 0;
+}
