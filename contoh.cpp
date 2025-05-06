952da8f38360cfce5a14202747a66620ce6f98a5
RizqiMaulana14 <maulanafitrah14@gmail.com>
2025-05-06 18:26:27 +0700

    Memasukkan member untuk persegi panjang

--- contoh.cpp
+++ contoh.cpp
@@ -2,5 +2,19 @@
 using namespace std;
 
 class PersegiPanjang{
+    public:                             //Akses Modifier
+    int panjang, lebar;
 
+    public:
+    void input(){
+        cout << "Masukkan Panjang : ";
+        cin >> panjang;
+        cout << "Masukkan Lebar : ";
+        cin >> lebar;
+    }
+
+    int luasPp (int a, int b){
+        return a * b;
+    }
+    
 }
